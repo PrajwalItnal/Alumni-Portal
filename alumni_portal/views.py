bb5cc513b653edcc96134f0a4b20428b7eb3da97
junedfattekhan5 <junedfattekhan5@gmail.com>
2026-02-27 13:26:05 +0530

    adding base template

--- alumni_portal/views.py
+++ alumni_portal/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to the Alumni Portal Home Page!")
+    return render(request, "home.html")
--- user/views.py
+++ user/views.py
@@ -5,3 +5,4 @@ from django.shortcuts import render
 
 def home(request):
     return HttpResponse("Welcome to the Alumni Portal User Page!")
+
