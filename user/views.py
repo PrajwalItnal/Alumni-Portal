5a8bd57703107ad7d81cdf54b2010a1868d53082
deepabl <deepalakkappagol98@gmail.com>
2026-02-28 13:25:45 +0530

    Updated user views logic

--- user/views.py
+++ user/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
-def home(request, register_id):
 
+def home(request, register_id):
+    
     return HttpResponse(f"Welcome to the Alumni Portal User Page, {register_id}!")
 
