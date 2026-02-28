51818d9ba0a61973c6f77ce82791441d0cb78c57
deepabl <deepalakkappagol98@gmail.com>
2026-02-28 13:28:42 +0530

    Nothing

--- user/views.py
+++ user/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
-def home(request, register_id):
-
-    return HttpResponse(f"Welcome to the Alumni Portal User Page, {register_id}!")
-
+def home(request):
+    register_id = request.session.get("register_id")
+    if not register_id:
+        return redirect("login")
+    else:
+        return HttpResponse(f"Welcome to your dashboard, {register_id}!")
