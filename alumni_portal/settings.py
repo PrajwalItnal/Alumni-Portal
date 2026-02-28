add05728c6bedabf0bb482f6214b3aef1cce9b1a
junedfattekhan5 <junedfattekhan5@gmail.com>
2026-02-28 12:40:10 +0530

    updating contact

--- alumni_portal/settings.py
+++ alumni_portal/settings.py
@@ -134,3 +134,13 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'junedfattekhan00@gmail.com'
+EMAIL_HOST_PASSWORD = 'yttt dizr ajgz efgp'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- alumni_portal/views.py
+++ alumni_portal/views.py
@@ -10,8 +10,69 @@ def home(request):
 def about(request):
     return render(request, "about.html")
 
+from django.core.mail import send_mail
+from django.conf import settings
+from django.shortcuts import render
+
 def contact(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        subject = "New Contact Message – Alumni Portal"
+
+        body = f"""
+            Dear Admin,
+
+            You have received a new contact message from the Alumni Portal website.
+
+            Name: {name}
+            Email: {email}
+
+            Message:
+            {message}
+
+            Thank you,
+            Alumni Portal System
+        """
+
+        send_mail(
+            subject,
+            body,
+            settings.EMAIL_HOST_USER,
+            ['junedfattekhan00@gmail.com'],  # Replace with your admin email
+            fail_silently=False,
+        )
+
+        user_subject = "Thank You for Contacting Us – Alumni Portal"
+        user_body = f"""
+            Dear {name},
+
+            Thank you for contacting the Alumni Portal.
+
+            We have received your message and our team will get back to you as soon as possible.
+
+            Here is a copy of your message:
+
+            {message}
+
+            Best Regards,
+            Alumni Portal Team
+        """
+
+        send_mail(
+            user_subject,
+            user_body,
+            settings.EMAIL_HOST_USER,
+            [email],  # Send to user
+            fail_silently=False,
+        )
+
+        return render(request, "contact.html", {"success": True})
+
     return render(request, "contact.html")
+
 
 def login(request):
     if request.method == "POST":
