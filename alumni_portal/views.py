7dfd0a779093d295227b12d48879e43ccc74ef75
Prajwal <prajwalitnal20@gmail.com>
2026-02-28 11:43:27 +0530

    Adding login technique

--- alumni_portal/views.py
+++ alumni_portal/views.py
@@ -1,5 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages
+
+from user.models import User
 
 def home(request):
     return render(request, "home.html")
@@ -11,4 +14,14 @@ def contact(request):
     return render(request, "contact.html")
 
 def login(request):
+    if request.method == "POST":
+        register_id = request.POST.get("register_id")
+        password = request.POST.get("password")
+        user = None
+        try:
+            user = User.objects.get(register_id=register_id, password=password)
+            return redirect('user:home')
+        except User.DoesNotExist:
+            messages.error(request, "Invalid register ID or password.")
+            return redirect('login')
     return render(request, "login.html")
