f1c283d9d73e7097053e765ab919be12ed1b4612
Prajwal <prajwalitnal20@gmail.com>
2026-02-28 15:27:30 +0530

    Adding the user admin and alumni home

--- user/views.py
+++ user/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 
+from user.models import User
+
 # Create your views here.
 def home(request):
     register_id = request.session.get("register_id")
     if not register_id:
         return redirect("login")
     else:
-        return render(request, "user/user_home.html", {"register_id": register_id})
+        user = User.objects.get(register_id=register_id)
+        if user.role == "Admin":
+            return render(request, "user/admin_home.html", {"user": user})
+        elif user.role == "Alumni":
+            return render(request, "user/alumni_home.html", {"user": user})
+        else:
+            return render(request, "user/user_home.html", {"user": user})
 
 def logout(request):
     request.session.flush()
