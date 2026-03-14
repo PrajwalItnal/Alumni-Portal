630f1fa9d6a63446b122a6a6ae87b5fcdd7c9f06
junedfattekhan5 <junedfattekhan5@gmail.com>
2026-03-14 15:51:29 +0530

    Updated settings file

--- alumni_portal/settings.py
+++ alumni_portal/settings.py
@@ -127,9 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 import os
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
